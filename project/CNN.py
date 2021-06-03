540b844ee735f8af1d54a378a51929d9d2100ef1
AbedShabaan <abed.shabaan@epfl.ch>
2021-06-03 14:45:48 +0200

    Commented CNN.py

--- project/CNN.py
+++ project/CNN.py
@@ -19,6 +19,9 @@ from sklearn.preprocessing import LabelEncoder
 class Classifier(nn.Module):
 
     def __init__(self):
+        """
+        A card image classifier that predicts both the digit and the suit using PyTorch
+        """
         super().__init__()
         self.features = nn.Sequential(
                             #nn.Dropout(0.3),
@@ -75,33 +78,50 @@ class Classifier(nn.Module):
         return symbol, suit
 
 
-def train_new_classifier(folder, verbose=False, save=False, validate=True, validation_game=7, mnist_samples=None):
+def train_new_classifier(folder:str, verbose=False, save=False, validate=True, validation_game=7, mnist_samples=None):
+    """
+    Trains a model of the class Classifier defined above, optionally adding some MNIST digits returns it with its training history and label encoders. Can be used to save the model or validate it as well
+    :param folder: Path to the folder containing the training images, those are binary masks of individual cards
+    :param verbose: Whether to print the training progress to the console
+    :param save: Whether to save the model's state_dict to a file
+    :param validate: Whether to validate the model, if True, game number validation_game will be excluded from training and used for evaluation
+    :param validation_game: Number of the game used for validation, has no effect if validate=False or if is not in range(1,8)
+    :param mnist_samples: The number of MNIST digits to add. None means none will be added
+    :return: model:Classifier: The trained classifier
+             hist:dict: The model's training history, has lists of losses and accuracies
+             encoders:dict A dictionary of LabelEncoders used to train this model, used to reverse the numerical labels to ones similar to what's in the training labels
+
+    """
+
+    validate = validate and validation_game in range(1,8)  # Check if the validation game index is correct
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     if verbose:
         print(f'Current device: {device}')
+        print(f'Validating? {validate}')
 
     if mnist_samples:
         if verbose:
             print(f'Adding {mnist_samples} samples from the MNIST dataset')
-        mnist = MNIST('data/MNIST', train=True)
-        idx = torch.randint(0, mnist.data.shape[0], (mnist_samples,))
-        mnist_images = torch.Tensor([resize(img_as_float(x), (100,100)) for x in mnist.data[idx]]).to(device)
+        mnist = MNIST('data/MNIST', train=True, download=True)
+        idx = torch.randint(0, mnist.data.shape[0], (mnist_samples,))  # A random sample of MNIST images
+        mnist_images = torch.Tensor([resize(img_as_float(x), (100,100)) for x in mnist.data[idx]]).to(device)  # Resizing the images and moving them to GPU if available
         mnist_images = mnist_images.unsqueeze(1)
         mnist_symbols = mnist.targets[idx].to(device).long()
-        mnist_suits = torch.zeros_like(mnist_symbols).to(device).long()  # Dummy label
-
-
+        mnist_suits = torch.zeros_like(mnist_symbols).to(device).long()  # Dummy label, causes problems if too many MNIST images
 
     model = Classifier().to(device)
     optimizer = Adam(params=model.parameters(), lr=1e-3)
     suit_loss = nn.CrossEntropyLoss()
     symbol_loss = nn.CrossEntropyLoss()
-    labels = produce_labels()
+
+    labels = produce_labels() #  Load the label Dataframe
     hist = {'loss':[], 'sym_acc':[], 'suit_acc':[], 'val_loss':[], 'val_sym_acc':[], 'val_suit_acc':[]}
+
     if verbose:
         print('Creating Tensors')
+
     if validate:
         images, symbols, suits, images_val, symbols_val, suits_val, symbol_encoder, suit_encoder = get_training_tensors(folder, labels, validation_game)
         images = images.to(device)
@@ -110,13 +130,16 @@ def train_new_classifier(folder, verbose=False, save=False, validate=True, valid
         images_val = images_val.to(device)
         symbols_val = symbols_val.to(device)
         suits_val = suits_val.to(device)
+
         if mnist_samples:
+            #  Add MNIST images to the dataset
             images = torch.cat((images, mnist_images), dim=0)
             symbols = torch.cat((symbols, mnist_symbols), dim=0)
             suits = torch.cat((suits, mnist_suits), dim=0)
         train_dataset = TensorDataset(images, symbols, suits)
         validation_dataset = TensorDataset(images_val, symbols_val, suits_val)
         validation_data = DataLoader(validation_dataset, batch_size=13)
+
     else:
         images, symbols, suits, symbol_encoder, suit_encoder = get_training_tensors(folder, labels, validation_game=None)
         images = images.to(device)
@@ -191,7 +214,14 @@ def train_new_classifier(folder, verbose=False, save=False, validate=True, valid
     return model, hist, encoders
 
 
-def get_training_tensors(folder, labels, validation_game=7):
+def get_training_tensors(folder:str, labels:pd.DataFrame, validation_game=7):
+    """
+    Creates PyTorch Tensors from the images present in the folder `folder` and the label DataFrame, can return one set of tensors for training or a training and validation set
+    :param folder: Path to the folder containing the training images
+    :param labels: DataFrame containing the compiled labels
+    :param validation_game: Index of the validation game, has to be between 1 and 7, otherwise all games are used for training
+    :return: Tensors in the order (images, symbols, suits, images_val, symbols_val, suits_val) and 2 encoders (symbol_encoder, label_encoder)
+    """
     files = [f'{folder}/{x}' for x in os.listdir(folder) if 'p' in x]
     images = []
     symbols = []
@@ -209,7 +239,7 @@ def get_training_tensors(folder, labels, validation_game=7):
 
         if game_id == validation_game:
             images_val.append(resize(img_as_float(imread(f)), output_shape=(100, 100)))
-            label = labels.loc[row_id, player_id].iloc[0]
+            label = labels.loc[row_id, player_id].iloc[0]  # loc returns a series because we are indexing rows by a boolean mask, so iloc[0] returns the value inside
             # print(label)
             symbols_val.append(label[0])
             suits_val.append(label[1])
@@ -222,12 +252,13 @@ def get_training_tensors(folder, labels, validation_game=7):
             suits.append(label[1])
 
     images = torch.Tensor(images).unsqueeze(1)
+    # Transform labels from characters ('0', 'Q', etc.) to numbers
     symbol_encoder = LabelEncoder()
     symbols = torch.from_numpy(symbol_encoder.fit_transform(symbols)).long()
     suit_encoder = LabelEncoder()
     suits = torch.from_numpy(suit_encoder.fit_transform(suits)).long()
 
-    if validation_game:
+    if validation_game in range(1,8):
         images_val = torch.Tensor(images_val).unsqueeze(1)
         symbols_val = torch.from_numpy(symbol_encoder.transform(symbols_val)).long()
         suits_val = torch.from_numpy(suit_encoder.transform(suits_val)).long()
@@ -238,6 +269,10 @@ def get_training_tensors(folder, labels, validation_game=7):
 
 
 def produce_labels():
+    """
+    Concatenate all label DataFrames into one
+    :return: The compiled label DataFrame
+    """
     label_root = './train_games'
     labels = pd.read_csv(f'{label_root}/game1/game1.csv')
     labels['game'] = 1
